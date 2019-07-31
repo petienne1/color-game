ce87fd4a7e26280ae4b847db448920f4c35c53b4
Pierry Etienne <pierry.etienne@gmail.com>
2019-07-30 22:26:52 -0400

    completed game functionality

--- colorGame.js
+++ colorGame.js
@@ -1,10 +1,56 @@
+let numSquares = 6;
 let colors = generateRandomColors(6);
-
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
+let easyBtn = document.querySelector("#easyBtn");
+let hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function() {
+  hardBtn.classList.remove("selected")
+  easyBtn.classList.add("selected");
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(let i = 0; i < squares.length; i++) {
+    if(colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+})
+
+hardBtn.addEventListener("click", function() {
+  hardBtn.classList.add("selected");
+  easyBtn.classList.remove("selected");
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(let i = 0; i < squares.length; i++) {
+      squares[i].style.backgroundColor = colors[i];
+      squares[i].style.display = "block";
+  }
+})
+
+resetButton.addEventListener("click", function() {
+  // gerate all new colors
+  colors = generateRandomColors(numSquares);
+  // pick a new random color from array
+  pickedColor = pickColor();
+  // changeColorDisplay to match picked color
+  colorDisplay.textContent = pickedColor;
+  // change colors of squares
+  for(let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -17,6 +63,7 @@ for(let i = 0; i < squares.length; i++) {
     let clickedColor = this.style.backgroundColor;
     // compare color to picked square
     if(clickedColor === pickedColor) {
+      resetButton.textContent = "Play Again?"
       messageDisplay.textContent = "Correct!";
       changeColors(clickedColor);
       h1.style.backgroundColor = clickedColor;
